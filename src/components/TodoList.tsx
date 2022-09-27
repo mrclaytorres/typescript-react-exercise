31c42a6155bb59f794c26b5a8bbc8c930fb5dd47
Algen <adelacruz@innovuze.com>
2022-09-27 20:59:34 +0800

    First commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './TodoList.css';
+
+interface TodoListProperties {
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList: React.FC<TodoListProperties> = (props) => {
+  return (
+    <ul>
+      {props.items.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
